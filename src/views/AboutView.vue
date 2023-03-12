f4faddba18fdd6a227496fa60dc990b4ecd153c8
emr3rden <emreerden@pm.me>
2023-03-13 00:01:27 +0300

    style: edit summary and hobbies section in AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,45 @@
 <script setup></script>
 
 <template>
-  <div class="about">
-    <h1>ABOUT</h1>
-  </div>
+  <main class="about">
+    <div class="summary text-lg">
+      First of all, Hello Mate! My name is Emre and I'm 22 years old. I live in
+      <a href="https://goo.gl/maps/cadu4sCBbzi4B6F98" class="about-link">Ankara/Turkey</a>. In the
+      last years of secondary school, my interest in the world of informatics started. I've been
+      learning since then. I continue to improve myself every day in the field of Front-End
+      Development.
+    </div>
+
+    <div class="hobbies flex flex-col gap-5">
+      <span class="select-none text-sm text-gray-400">What I like to do and my hobbies are:</span>
+      <ul class="flex flex-col gap-8">
+        <li>
+          I love watching Movies and TV Series. And I like to talk to people about them. If you want
+          to see the movies I watched and my watch list:
+          <a target="_blank" href="https://letterboxd.com/emr3rden/" class="about-link"
+            >My Letterboxd Account</a
+          >
+        </li>
+        <li>
+          I love playing computer games since childhood. It's something I can never give up. If you
+          are interested in games, you can check
+          <a target="_blank" href="https://steamcommunity.com/id/WiaMoria" class="about-link"
+            >My Steam Account</a
+          >
+          for the games I play and add me as a friend.
+        </li>
+        <li>
+          Also, I have to say that finding new songs and creating playlists on Spotify are among my
+          hobbies. You can check
+          <a
+            target="_blank"
+            href="https://open.spotify.com/user/gb0jo18xvox7w4gmijbj3l301?si=089ed329e6da4203"
+            class="about-link"
+            >My Spotify Account</a
+          >
+          for playlists.
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
