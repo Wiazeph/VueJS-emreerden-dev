d7d94ca4ba447ab528d3a9f36982b016dc3cc693
emr3rden <emreerden@pm.me>
2023-03-12 21:45:56 +0300

    style: edit HomeView and Navigation

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -3,13 +3,15 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav class="nav-bar">
-    <RouterLink to="/">Hello</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/skills">Skills</RouterLink>
-    <RouterLink to="/projects">Projects</RouterLink>
-    <RouterLink to="/tools">Tools</RouterLink>
-  </nav>
+  <header>
+    <nav class="nav-bar">
+      <RouterLink to="/" class="nav-bar-link">Hello</RouterLink>
+      <RouterLink to="/about" class="nav-bar-link">About</RouterLink>
+      <RouterLink to="/skills" class="nav-bar-link">Skills</RouterLink>
+      <RouterLink to="/projects" class="nav-bar-link">Projects</RouterLink>
+      <RouterLink to="/tools" class="nav-bar-link">Tools</RouterLink>
+    </nav>
+  </header>
 
   <RouterView />
 </template>
