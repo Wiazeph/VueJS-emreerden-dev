bd19cf641235bd50d46761758dd53d718b3c635b
emr3rden <emreerden@pm.me>
2023-03-14 01:30:39 +0300

    feat: add projects using fetch api from github repos with github api

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,63 @@
-<script setup></script>
-
 <template>
-  <div class="projects">
-    <h1>PROJECTS</h1>
+  <div>
+    <div class="repo">
+      <h2>Name: {{ repos[0].name }}</h2>
+      <p>Stars: {{ repos[0].stars }}</p>
+    </div>
+    <div class="repo">
+      <h2>Name: {{ repos[1].name }}</h2>
+      <p>Stars: {{ repos[1].stars }}</p>
+    </div>
   </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const repos = ref([
+      {
+        name: 'Loading...',
+        stars: 'Loading...'
+      },
+      {
+        name: 'Loading...',
+        stars: 'Loading...'
+      }
+    ])
+
+    const getRepoData = async () => {
+      const movieSearchResponse = await fetch(
+        'https://api.github.com/repos/emr3rden/VueJS-Movie-Search'
+      )
+      const movieSearchData = await movieSearchResponse.json()
+
+      const movieSearch = {
+        name: movieSearchData.name,
+        stars: movieSearchData.stargazers_count
+      }
+
+      const userCardsResponse = await fetch(
+        'https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards'
+      )
+      const userCardsData = await userCardsResponse.json()
+
+      const userCards = {
+        name: userCardsData.name,
+        stars: userCardsData.stargazers_count
+      }
+
+      repos.value = [movieSearch, userCards]
+    }
+
+    onMounted(() => {
+      getRepoData()
+    })
+
+    return {
+      repos
+    }
+  }
+}
+</script>
