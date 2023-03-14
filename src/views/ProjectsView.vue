27c66b54a0f5018e51db9eb6ff793cec3afedcba
emr3rden <emreerden@pm.me>
2023-03-14 18:42:20 +0300

    refactor: change fetch api to use Promise.all

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="repo">
-      <h2>Name: {{ repos[0].name }}</h2>
-      <p>Stars: {{ repos[0].stars }}</p>
+    <div v-for="repo in repos" :key="repo.id" class="project">
+      <h2>Project Name: {{ repo.name }}</h2>
+      <p>Project Star Number: {{ repo.stars }}</p>
     </div>
-    <div class="repo">
-      <h2>Name: {{ repos[1].name }}</h2>
-      <p>Stars: {{ repos[1].stars }}</p>
+
+    <div v-if="!repos || repos.length === 0" class="text-xl text-pink-400 sm:text-2xl md:text-4xl">
+      Loading...
     </div>
   </div>
 </template>
@@ -16,39 +16,29 @@ import { ref, onMounted } from 'vue'
 
 export default {
   setup() {
-    const repos = ref([
-      {
-        name: 'Loading...',
-        stars: 'Loading...'
-      },
-      {
-        name: 'Loading...',
-        stars: 'Loading...'
-      }
-    ])
+    const repos = ref([])
 
     const getRepoData = async () => {
-      const movieSearchResponse = await fetch(
-        'https://api.github.com/repos/emr3rden/VueJS-Movie-Search'
+      const [movieSearchResponse, userCardsResponse] = await Promise.all([
+        fetch('https://api.github.com/repos/emr3rden/VueJS-Movie-Search'),
+        fetch('https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards')
+      ])
+
+      const [movieSearchData, userCardsData] = await Promise.all([
+        movieSearchResponse.json(),
+        userCardsResponse.json()
+      ])
+
+      repos.value.push(
+        {
+          name: movieSearchData.name,
+          stars: movieSearchData.stargazers_count
+        },
+        {
+          name: userCardsData.name,
+          stars: userCardsData.stargazers_count
+        }
       )
-      const movieSearchData = await movieSearchResponse.json()
-
-      const movieSearch = {
-        name: movieSearchData.name,
-        stars: movieSearchData.stargazers_count
-      }
-
-      const userCardsResponse = await fetch(
-        'https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards'
-      )
-      const userCardsData = await userCardsResponse.json()
-
-      const userCards = {
-        name: userCardsData.name,
-        stars: userCardsData.stargazers_count
-      }
-
-      repos.value = [movieSearch, userCards]
     }
 
     onMounted(() => {
