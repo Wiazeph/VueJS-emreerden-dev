7914e48b821f3a87a1c1f1eb5717b2ca06bd233f
emr3rden <emreerden@pm.me>
2023-03-15 20:02:50 +0300

    feat: add two new repo data

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -19,24 +19,42 @@ export default {
     const repos = ref([])
 
     const getRepoData = async () => {
-      const [movieSearchResponse, userCardsResponse] = await Promise.all([
+      const [resourcesResponse, movieSearchResponse, userCardsResponse] = await Promise.all([
+        fetch('https://api.github.com/repos/emr3rden/Front-End-Development-Resources'),
         fetch('https://api.github.com/repos/emr3rden/VueJS-Movie-Search'),
         fetch('https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards')
       ])
 
-      const [movieSearchData, userCardsData] = await Promise.all([
+      const [resourcesData, movieSearchData, userCardsData] = await Promise.all([
+        resourcesResponse.json(),
         movieSearchResponse.json(),
         userCardsResponse.json()
       ])
 
       repos.value.push(
         {
-          name: movieSearchData.name,
-          stars: movieSearchData.stargazers_count
+          url: resourcesData.html_url,
+          live: resourcesData.homepage,
+          name: resourcesData.name,
+          desc: resourcesData.description,
+          stars: resourcesData.stargazers_count,
+          img: 'https://github.com/emr3rden/Front-End-Development-Resources/blob/main/thumbnail/Front-End-Development-Resources.png?raw=true'
         },
         {
+          url: movieSearchData.html_url,
+          live: movieSearchData.homepage,
+          name: movieSearchData.name,
+          desc: movieSearchData.description,
+          stars: movieSearchData.stargazers_count,
+          img: 'https://github.com/emr3rden/VueJS-Movie-Search/blob/main/thumbnail/VueJS-Movie-Search.png?raw=true'
+        },
+        {
+          url: userCardsData.html_url,
+          live: userCardsData.homepage,
           name: userCardsData.name,
-          stars: userCardsData.stargazers_count
+          desc: userCardsData.description,
+          stars: userCardsData.stargazers_count,
+          img: 'https://github.com/emr3rden/VueJS-User-Profile-Cards/blob/main/thumbnail/VueJS-User-Profile-Cards.png?raw=true'
         }
       )
     }
