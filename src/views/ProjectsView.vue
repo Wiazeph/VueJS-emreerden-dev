8a05250d41caf19a0adba1713a1c79bf0cc2b1a3
Emre Erden <emreerden@pm.me>
2023-05-20 00:16:41 +0300

    refactor: change script setup

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -78,61 +78,53 @@
   </main>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted } from 'vue'
 
-export default {
-  setup() {
-    const repos = ref([])
+const repos = ref([])
 
-    const getRepoData = async () => {
-      const [resourcesResponse, movieSearchResponse, userCardsResponse] = await Promise.all([
-        fetch('https://api.github.com/repos/emr3rden/Front-End-Development-Resources'),
-        fetch('https://api.github.com/repos/emr3rden/VueJS-Movie-Search'),
-        fetch('https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards')
-      ])
+const getRepoData = async () => {
+  const [resourcesResponse, movieSearchResponse, userCardsResponse] = await Promise.all([
+    fetch('https://api.github.com/repos/emr3rden/Front-End-Development-Resources'),
+    fetch('https://api.github.com/repos/emr3rden/VueJS-Movie-Search'),
+    fetch('https://api.github.com/repos/emr3rden/VueJS-User-Profile-Cards')
+  ])
 
-      const [resourcesData, movieSearchData, userCardsData] = await Promise.all([
-        resourcesResponse.json(),
-        movieSearchResponse.json(),
-        userCardsResponse.json()
-      ])
+  const [resourcesData, movieSearchData, userCardsData] = await Promise.all([
+    resourcesResponse.json(),
+    movieSearchResponse.json(),
+    userCardsResponse.json()
+  ])
 
-      repos.value.push(
-        {
-          url: resourcesData.html_url,
-          live: resourcesData.homepage,
-          name: resourcesData.name,
-          desc: resourcesData.description,
-          stars: resourcesData.stargazers_count,
-          img: 'https://github.com/emr3rden/Front-End-Development-Resources/blob/main/thumbnail/Front-End-Development-Resources.png?raw=true'
-        },
-        {
-          url: movieSearchData.html_url,
-          live: movieSearchData.homepage,
-          name: movieSearchData.name,
-          desc: movieSearchData.description,
-          stars: movieSearchData.stargazers_count,
-          img: 'https://github.com/emr3rden/VueJS-Movie-Search/blob/main/thumbnail/VueJS-Movie-Search.png?raw=true'
-        },
-        {
-          url: userCardsData.html_url,
-          live: userCardsData.homepage,
-          name: userCardsData.name,
-          desc: userCardsData.description,
-          stars: userCardsData.stargazers_count,
-          img: 'https://github.com/emr3rden/VueJS-User-Profile-Cards/blob/main/thumbnail/VueJS-User-Profile-Cards.png?raw=true'
-        }
-      )
+  repos.value.push(
+    {
+      url: resourcesData.html_url,
+      live: resourcesData.homepage,
+      name: resourcesData.name,
+      desc: resourcesData.description,
+      stars: resourcesData.stargazers_count,
+      img: 'https://github.com/emr3rden/Front-End-Development-Resources/blob/main/thumbnail/Front-End-Development-Resources.png?raw=true'
+    },
+    {
+      url: movieSearchData.html_url,
+      live: movieSearchData.homepage,
+      name: movieSearchData.name,
+      desc: movieSearchData.description,
+      stars: movieSearchData.stargazers_count,
+      img: 'https://github.com/emr3rden/VueJS-Movie-Search/blob/main/thumbnail/VueJS-Movie-Search.png?raw=true'
+    },
+    {
+      url: userCardsData.html_url,
+      live: userCardsData.homepage,
+      name: userCardsData.name,
+      desc: userCardsData.description,
+      stars: userCardsData.stargazers_count,
+      img: 'https://github.com/emr3rden/VueJS-User-Profile-Cards/blob/main/thumbnail/VueJS-User-Profile-Cards.png?raw=true'
     }
-
-    onMounted(() => {
-      getRepoData()
-    })
-
-    return {
-      repos
-    }
-  }
+  )
 }
+
+onMounted(() => {
+  getRepoData()
+})
 </script>
